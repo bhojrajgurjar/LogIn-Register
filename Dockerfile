7cd159e035b1741a229dded29d4ee983fc11ca8f
bhojrajgurjar <bgurjar2000@gmail.com>
2025-09-20 16:37:08 +0530

    changes for deploy

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+# Use an official Tomcat image
+FROM tomcat:9.0
+
+# Remove default webapps
+RUN rm -rf /usr/local/tomcat/webapps/*
+
+# Copy your WAR file to Tomcat webapps folder
+COPY Login-Register.war /usr/local/tomcat/webapps/ROOT.war
+
+# Expose port 8080
+EXPOSE 8080
+
+# Start Tomcat
+CMD ["catalina.sh", "run"]
--- src/main/java/com/company/connection/DbDetails.java
+++ src/main/java/com/company/connection/DbDetails.java
@@ -1,10 +1,9 @@
 package com.company.connection;
 
 public interface DbDetails {
-	String URL = "jdbc:mysql://localhost:3306/"
-			+ "logInUser";
-	String USERNAME = "root";
-	String PASSWORD = "root";
+	String URL = System.getenv("DB_URL");
+	String USERNAME = System.getenv("DB_USER");
+	String PASSWORD = System.getenv("DB_PASS");
 	String DBDRIVER = "com.mysql.cj.jdbc.Driver";
 
 }
